3adbf829998cdc92fa7d968d38e3984fa7792390
kotobusdriver <utkins@gmail.com>
2025-05-22 10:45:44 +0300

    implement student

--- src/main/java/org/example/app/entity/Student.java
+++ src/main/java/org/example/app/entity/Student.java
@@ -1,18 +1,99 @@
 package org.example.app.entity;
 
-public class Student {
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
+
+@Entity
+@Table(name="student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+    @Column(name="fist_name", nullable=false)
     private String firstName;
+    @Column (name="last_name", nullable=false)
     private String lastName;
+    @Column (name="email", nullable=false, unique=true)
     private String email;
+
+    @OneToMany(
+        mappedBy = "student",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true
+    )
     private Set<Homework> homeworks = new HashSet<>();
 
+    public Student() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Homework> getHomeworks() {
+        return homeworks;
+    }
+
+    public void setHomeworks(Set<Homework> homeworks) {
+        this.homeworks = homeworks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student other = (Student) o;
+        return id != null && Objects.equals(id, other.getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", homeworksCount=" + homeworks.size() +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
     public void addHomework(final Homework homework) {
-        // TODO implement this method
+        homeworks.add(homework);
+        homework.setStudent(this);
     }
 
     public void removeHomework(final Homework homework) {
-        // TODO implement this method
+        homeworks.remove(homework);
+        homework.setStudent(null);
     }
 }
