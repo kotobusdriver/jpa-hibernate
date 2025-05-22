d834294e89c07c69fbd9cc5a120523f4083058ae
kotobusdriver <utkins@gmail.com>
2025-05-22 11:07:10 +0300

    implement homework

--- src/main/java/org/example/app/entity/Homework.java
+++ src/main/java/org/example/app/entity/Homework.java
@@ -1,10 +1,75 @@
 package org.example.app.entity;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table (name = "homework")
 public class Homework {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column (nullable = false)
     private String description;
+    @Column (nullable = false)
     private LocalDate deadline;
     private int mark;
+
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn (name = "student_id", nullable = false)
     private Student student;
+
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Homework homework = (Homework) o;
+        return Objects.equals(id, homework.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Homework{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", deadline=" + deadline +
+                ", mark=" + mark +
+                ", assignedToStudent=" + student +
+                '}';
+    }
 }
